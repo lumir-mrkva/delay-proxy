af3f392fbc568ac6b1d49b7e92e40ec748650fc1
Lumir Mrkva <lumir.mrkva@topmonks.com>
2015-05-25 20:41:41 +0200

    delay proxy

---
+++ delay-proxy.js
@@ -0,0 +1,28 @@
+var http = require('http'),
+    httpProxy = require('http-proxy'),
+    args = require('minimist')(process.argv.slice(2));
+
+var proxy = httpProxy.createProxyServer({}),
+    to = args._[0] || 'http://localhost';
+
+var server = http.createServer(function(req, res) {
+  var url = req.url,
+      delay = 0;
+  if (url) {
+    tokens = url.split('/');
+    tokens.shift();
+    delay = parseInt(tokens.shift()) || delay;
+    if (delay) {
+      req.url = '/' + tokens.join('/');
+    }
+  }
+  console.log(req.method + '('+delay+') ' +to + req.url);
+  setTimeout(function () {
+    proxy.web(req, res, {
+      target: to
+    });
+  }, delay);
+});
+
+console.log("listening on port 5050")
+server.listen(5050);
